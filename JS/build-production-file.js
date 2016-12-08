6f3002ca2acc0ab86edd747d595f9b0258e86e87
Daniel Rieck <danielcrieck@gmail.com>
2016-12-08 15:40:50 -0800

    Correcting typo in filename, adding build dependency
    Addressing a copy/paste mistake in site-specific custom JS file name, adding cookie objects plugin and accompanying application to build order.

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -2,9 +2,10 @@
 var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
 concat([
  '../WSU-UE---JS/jQuery.oue-custom.js',
- './ucore-assessment-custom.js',
+ './ucore-custom.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.textResize.js',
+ '../WSU-UE---JS/jQuery.cookieObjs.js',
  '../WSU-UE---JS/jQuery.masonry.min.js'
  ], './wp-custom-js-source.js', function() {
     console.log('Concatenation complete.');     
