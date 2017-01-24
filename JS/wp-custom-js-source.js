9da27c565fa9f22bd55368b76244c1c55c41c577
Daniel Rieck <danielcrieck@gmail.com>
2017-01-24 13:52:12 -0800

    Updating build
    Rebuilt production files to incorporate recent changes to source code.

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -396,8 +396,8 @@
 		}
 		
 		// Add initially hidden (via CSS) expand/collapse all buttons to definition lists
-		$lists.prepend("<div class=""collapse-all-button"">Collapse All</div>");
-		$lists.prepend("<div class=""expand-all-button"">Expand All</div>");
+		$lists.prepend('<div class="collapse-all-button">Collapse All</div>');
+		$lists.prepend('<div class="expand-all-button">Expand All</div>');
 		var slctrExpandAll = slctrDefList + expandAllClass;
 		var $expandAlls = $(slctrExpandAll);
 		var slctrCollapseAll = slctrDefList + collapseAllClass;
