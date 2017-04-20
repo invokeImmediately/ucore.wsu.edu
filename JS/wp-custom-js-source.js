2c30fdeeba9fb785b2d3d5c6f32926953e38d51d
Daniel Rieck <danielcrieck@gmail.com>
2017-04-19 21:18:49 -0700

    Updating build
    Rebuilt production files to incorporate recent changes to source code.

--- JS/wp-custom-js-source.js
+++ JS/wp-custom-js-source.js
@@ -37,7 +37,7 @@
 		bitMask = (typeof fnctnDesc === "string") | (bitMask << 1);
 		bitMask = (typeof errorMsg === "string") | (bitMask << 1);
 		if (bitMask === 15) {
-			console.log("error = {\n\tfile: '" + fileName + "',\n\tfunctionName: '" + fnctnName + "'\n\terrorMessage: '" + fnctnDesc + "'\n\terrorMessage: '" + errorMsg + "'\n\t};");
+			console.log("error = {\n\tfile: '" + fileName + "',\n\tfunctionName: '" + fnctnName + "'\n\tfunctionDesc: '" + fnctnDesc + "'\n\terrorMessage: '" + errorMsg + "'\n\t};");
 		} else {
 			var incorrectTypings;
 			var bitMaskCopy;
